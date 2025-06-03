6a207ae526f83d91dc55e1ec4fad214565d30762
Carlos Emmanuel Arroyo Torres <ceat1989.ca@gmail.com>
2025-06-02 23:53:22 -0600

    Add files via upload

---
+++ logica.js
@@ -0,0 +1,9 @@
+function agregarCompra(){
+    alert('Has agregado el producto al Carrito, sigue comprando. Muchas Gracias, Att: Besso Joyería.')
+}
+function formularioEnviar01(){
+    alert('Tu pedido esta en camino. Muchas Gracias, Att: Besso Joyería.')
+}
+function formularioEnviar02(){
+    alert('Tu formulario ha sido enviado, pronto nos podremos en contacto. Att: Besso Joyería')
+}
